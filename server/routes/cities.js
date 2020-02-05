8b0d8a6e5f65a04068f626eb88f877a9ba8d113e
Renato Acciardi <a.renato@gmail.com>
2020-02-05 16:39:41 +0100

    Sprint 2 done

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const cityModel = require("../model/cityModel");
 
+// get first 12 popular itineraries
+router.get("/carousel", (req, res) => {
+  cityModel
+    .find({}, null, {limit: 12, sort: {'name': 1}})
+    .then(files => {
+      res.send(files);
+    })
+    .catch(err => console.log(err));
+});
+
 /*get all cities*/
 router.get("/all", (req, res) => {
   cityModel
@@ -14,35 +24,34 @@ router.get("/all", (req, res) => {
 
 /*add a City if not existing already CREATE*/
 router.post("/", (req, res) => {
-    cityModel.findOne({ name: req.body.name })
-    .then (exists => {
-        if (exists) {
-            console.log("Cittá esistente", exists);
-            res.send("ESISTE");
-        } else {
-            const newCity = new cityModel({
-                name: req.body.name,
-                country: req.body.country,
-                url: req.body.url
-            });
-            console.log("Cittá aggiunta");
-            newCity
-            .save()
-            .then(city => {
-            res.send(city);
-            })
-            .catch(err => {
-            console.log("ERRORE CITTÀ ESISTENTE");
-            res.status(500).send(err);
-            });
-        };
-    });
-})
+  cityModel.findOne({ name: req.body.name }).then(exists => {
+    if (exists) {
+      console.log("Cittá esistente", exists);
+      res.send("ESISTE");
+    } else {
+      const newCity = new cityModel({
+        name: req.body.name,
+        country: req.body.country,
+        url: req.body.url
+      });
+      console.log("Cittá aggiunta");
+      newCity
+        .save()
+        .then(city => {
+          res.send(city);
+        })
+        .catch(err => {
+          console.log("ERRORE CITTÀ ESISTENTE");
+          res.status(500).send(err);
+        });
+    }
+  });
+});
 
 /*get one city READ*/
 router.get("/:name", (req, res) => {
   cityModel
-    .find({name:req.params.name})
+    .find({ name: req.params.name })
     .then(files => {
       console.log(files);
       res.send(files);
@@ -52,25 +61,28 @@ router.get("/:name", (req, res) => {
 
 /*update one city UPDATE*/
 router.put("/:name/:newname", (req, res) => {
-  cityModel.findOneAndUpdate(
+  cityModel
+    .findOneAndUpdate(
       { name: req.params.name },
-      { $set: {
-        name: req.params.newname }
-      })
-      .then(old => {
-        if (old !== null) {
-          res.json(old);
-          console.log("Città aggiornata")
-        } else {
-          console.log("ERRORE AGGIORNAMENTO");
-          res.json(old);
-        };
-      })
-  });
+      {
+        $set: {
+          name: req.params.newname
+        }
+      }
+    )
+    .then(old => {
+      if (old !== null) {
+        res.json(old);
+        console.log("Città aggiornata");
+      } else {
+        console.log("ERRORE AGGIORNAMENTO");
+        res.json(old);
+      }
+    });
+});
 
-  router.delete("/:name", (req, res) => {
-    cityModel.findOneAndDelete({ name: req.params.name })
-    .then(result => {
+router.delete("/:name", (req, res) => {
+  cityModel.findOneAndDelete({ name: req.params.name }).then(result => {
     if (result) {
       console.log("Successful deletion");
       res.send(result);
@@ -79,13 +91,6 @@ router.put("/:name/:newname", (req, res) => {
       res.send(result);
     }
   });
-})
-
-// router.get('/test',
-//     (req, res) => {
-//         res.send({
-//             msg: "Hola funciona"
-//         })
-//     })
+});
 
 module.exports = router;
