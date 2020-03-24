b2c83dda94196606b92018505c98fe0186b3867a
Renato Acciardi <a.renato@gmail.com>
2020-03-24 16:30:44 +0100

    Post Heroku

--- server/client/src/components/SearchBar.js
+++ server/client/src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+
 import { setCampoCerca } from "../store/actions/appActions";
 
 import { connect } from "react-redux";
